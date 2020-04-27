b74b8dd011b858c98854d89da4f401e176e23572
Brian Fajardo <bmf5@hotmail.com>
2020-04-27 18:45:34 -0400

    Initial Commit

---
+++ swiftUI-warCardGame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftUI-warCardGame
+//
+//  Created by Brian Fajardo on 2020-04-27.
+//  Copyright © 2020 Brian Fajardo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
